d559a302da4a3ff1e194f256cd1e3eaafcbb2ca6
Ubuntu <ubuntu@ip-172-31-48-139.ec2.internal>
2020-03-20 17:47:23 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -13,4 +13,18 @@ resource "aws_s3_bucket" "flugel" {
   }
 }
 
+resource "aws_s3_bucket_object" "object_1" {
+  bucket = "${aws_s3_bucket.flugel.id}"
+  key    = "test1.txt"
+        content = "${timestamp()}"
+}
 
+resource "aws_s3_bucket_object" "object_2" {
+  bucket = "${aws_s3_bucket.flugel.id}"
+  key    = "test2.txt"
+        content = "${timestamp()}"
+}
+
+output "bucket_id" {
+  value = "${aws_s3_bucket.flugel.id}"
+}
