f3dab9a63ce5c0356149406bb5bd6528ae79d142
Cesar Amaya <cesar.amaya+flugel@gmail.com>
2020-03-26 19:32:37 +0000

    vpc and security groups

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+# S3 bucket
 resource "aws_s3_bucket" "flugel" {
   bucket = "${var.bucket_name}"
   acl    = "private"
@@ -12,19 +13,46 @@ resource "aws_s3_bucket" "flugel" {
     Environment = "Dev"
   }
 }
-
+# S3 Objects
 resource "aws_s3_bucket_object" "object_1" {
   bucket = "${aws_s3_bucket.flugel.id}"
   key    = "test1.txt"
         content = "${timestamp()}"
 }
-
 resource "aws_s3_bucket_object" "object_2" {
   bucket = "${aws_s3_bucket.flugel.id}"
   key    = "test2.txt"
         content = "${timestamp()}"
 }
 
-output "bucket_id" {
-  value = "${aws_s3_bucket.flugel.id}"
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  
+  tags = {
+    Name = "Flugel"
+  }
+}
+
+# Security Groups
+resource "aws_security_group" "alb_sg" {
+  name          = "${var.alb_sg_name}"
+  vpc_id        = "${aws_vpc.main.id}"
+  tags = {
+    Name = "flugel_allow_http"
+  }
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
 variable "bucket_name" {
   default = "flugel-test1-bucket"
 }
+variable "alb_sg_name" {
+  default = "flugel-alb-sg-1"
+}
